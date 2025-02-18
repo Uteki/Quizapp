4d7efa3be85735785369e93ca15e0453ec20bc1f
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-02-18 22:58:01 +0100

    next question button and some other stuff

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 let currentQuestion = 0;
 
 function init() {
-    document.getElementById("max-count").innerHTML = questions.length;
+    let maxNum = document.querySelectorAll(".max-count");
+    maxNum.forEach(num => {
+        num.innerHTML = questions.length;
+    })
+
+    document.getElementById("endScreen").classList.add("d_none");
 
     showQuestion(currentQuestion);
 }
 
 function showQuestion(currentQuestion) {
-    let question = questions[currentQuestion];
+    if (currentQuestion >= questions.length) {
+        document.getElementById("endScreen").classList.toggle("d_none");
+        document.getElementById("startScreen").classList.toggle("d_none");
+    } else {
+        let question = questions[currentQuestion];
 
-    document.getElementById("questiontext").innerHTML = question['question'];
-    document.getElementById("answer_1").innerHTML = question['answer_1'];
-    document.getElementById("answer_2").innerHTML = question['answer_2'];
-    document.getElementById("answer_3").innerHTML = question['answer_3'];
-    document.getElementById("answer_4").innerHTML = question['answer_4'];
+        document.getElementById('curr-count').innerHTML = currentQuestion +1;
+
+        document.getElementById("questiontext").innerHTML = question['question'];
+        document.getElementById("answer_1").innerHTML = question['answer_1'];
+        document.getElementById("answer_2").innerHTML = question['answer_2'];
+        document.getElementById("answer_3").innerHTML = question['answer_3'];
+        document.getElementById("answer_4").innerHTML = question['answer_4'];
+    }
+
 }
 
 function answer(answer) {
@@ -21,13 +34,23 @@ function answer(answer) {
     let selectedQuestionNumber = answer.slice(-1);
 
     if (selectedQuestionNumber == question['right_answer']) {
-        console.log(true);
         document.getElementById(answer).parentNode.classList.add('bg-success');
     } else {
-        console.log(false);
         document.getElementById(answer).parentNode.classList.add('bg-danger');
         document.getElementById(`answer_` + question['right_answer']).parentNode.classList.add('bg-success');
     }
 
     document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+
+    for (i = 1; i <= 4; i++) {
+        document.getElementById(`answer_${i}`).parentNode.classList.remove('bg-success');
+        document.getElementById(`answer_${i}`).parentNode.classList.remove('bg-danger');
+    }
+
+    showQuestion(currentQuestion);
 }
